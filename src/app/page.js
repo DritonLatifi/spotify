18a9155303e82f366c29e5a0b7f472655e0034a3
Driton <106562685+DritonLatifi@users.noreply.github.com>
2024-05-29 21:13:41 +0000

    slider finished with static data

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,15 @@
+"use client"
+
+import Slider from "@/components/slider";
+
 export default function Home() {
-  return <div></div>
+  const dummyArray = new Array(5).fill('Kendrick Lamar')
+
+  return (
+    <main>
+      <Slider heading={'songs'} arr={dummyArray} />
+      <Slider heading={'playlist'} arr={dummyArray} />
+      <Slider heading={'albums'} arr={dummyArray} />
+    </main>
+  )
 }
---
+++ src/components/slider.js
@@ -0,0 +1,21 @@
+"use client"
+
+import Image from "next/image";
+
+export default function Slider({ heading, arr }) {
+    return (
+        <article className="flex flex-col gap-4 pl-4 pt-4">
+            <h2 className="font-bold text-3xl capitalize">{heading}</h2>
+            <div className="flex gap-2 overflow-x-scroll">
+                {arr.map((track, index) => {
+                    return (
+                        <div key={index} className="min-w-[150px] text-center">
+                            <Image src='/kendrick.webp' alt="image" className="w-full object-cover" height={150} width={150} />
+                            <span className="font-bold text-lg">{track}</span>
+                        </div>
+                    )
+                })}
+            </div>
+        </article>
+    )
+}
