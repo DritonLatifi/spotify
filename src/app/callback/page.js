5e86d1e84d12cec5e17b7ded2d47ae5a62955936
chrille0234 <madsen.chr@hotmail.com>
2024-06-08 02:44:41 +0200

    Add subscription tier to cookies

--- src/app/callback/page.js
+++ src/app/callback/page.js
@@ -1,17 +1,36 @@
 "use client"
 
-import { redirect } from "next/navigation"
 import { useEffect } from "react"
+import Cookies from "cookies-js"
 
 export default function Callback({ searchParams }) {
     useEffect(() => {
         fetch('api/login?code=' + searchParams.code)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => {
+               if(data.data?.access_token){
+                   let {access_token} = data.data
+                   checkIfPremium(access_token)
+
+               }
+            })
     }, [])
 
+    async function checkIfPremium(token){
+        const req = await fetch("https://api.spotify.com/v1/me", {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
 
-    redirect('/')
+        const res = await req.json();
+
+        Cookies.set("tier", res.product)
+
+    }
+
+
 
     return null
 }
