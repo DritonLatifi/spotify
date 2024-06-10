fdc6260010e3b0658439921fca7406d4f98983f6
Michelle Duineveld <michellemei29@gmail.com>
2024-06-10 13:06:03 +0200

    small aesthetic fix

--- src/components/playlistContent.js
+++ src/components/playlistContent.js
@@ -92,8 +92,8 @@ export default function PlaylistContent() {
     <>
       <div className="flex justify-between items-center mt-3 relative w-full">
         {/* Playlist title should be displayed here */}
-        <h1 className="text-2xl font-bold ml-[5%]">My Playlist 12</h1>
-        <button className="ml-auto p-2 pr-5" aria-label="Close">
+        <h1 className="text-2xl font-bold ml-[5%] pb-4">My Playlist 12</h1>
+        <button className="ml-auto p-2 pr-5 pb-5" aria-label="Close">
           <Image
             src="/close_icon.webp"
             height={25}
