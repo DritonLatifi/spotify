9e18097c9fe7e954de2902a1752c30eae0b70986
Driton <106562685+DritonLatifi@users.noreply.github.com>
2024-06-10 11:08:27 +0000

    callback endpoint cleanup

--- src/app/callback/page.js
+++ src/app/callback/page.js
@@ -3,37 +3,17 @@
 import { useEffect } from "react"
 import { useRouter } from 'next/navigation'
 
-import Cookies from "cookies-js"
-
 export default function Callback({ searchParams }) {
     const router = useRouter()
 
     useEffect(() => {
         fetch('api/login?code=' + searchParams.code)
-            .then(res => res.json())
-            .then(data => {
-               if(data.data?.access_token){
-                   let {access_token} = data.data
-                   checkIfPremium(access_token)
-
-               }
+            .then(res => {
+                if (res.status !== 200) {
+                    console.log("Login Failed")
+                }
             })
     }, [])
-
-    async function checkIfPremium(token){
-        const req = await fetch("https://api.spotify.com/v1/me", {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-
-        const res = await req.json();
-
-        Cookies.set("tier", res.product)
-
-    }
-
 
     // router.push('/')
 
