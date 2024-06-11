27fa87961ac3795355ed2cf3cc95ac3f10c7f93b
Driton <106562685+DritonLatifi@users.noreply.github.com>
2024-06-11 08:59:08 +0000

    success on login

--- src/app/callback/page.js
+++ src/app/callback/page.js
@@ -1,19 +1,18 @@
 "use client"
 
-import { useEffect } from "react"
 import { useRouter } from 'next/navigation'
 
 export default function Callback({ searchParams }) {
     const router = useRouter()
 
-    useEffect(() => {
-        fetch('api/login?code=' + searchParams.code)
-            .then(res => {
-                if (res.status !== 200) {
-                    console.log("Login Failed")
-                }
-            })
-    }, [])
+    fetch('api/login?code=' + searchParams.code)
+        .then(res => {
+            if (res.status !== 200) {
+                console.log("Login Failed")
+            } else {
+                console.log('Login Successful');
+            }
+        })
 
     router.push('/')
 
