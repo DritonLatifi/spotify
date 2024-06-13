ee1fb6adc01c6206a71fcfd84093e5046d123644
DritonLatifi <driton.latifi101@gmail.com>
2024-06-13 23:49:56 +0200

    searchbar fixed

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -4,20 +4,13 @@ import { IoSearch } from "react-icons/io5";
 
 export default function SearchBar() {
   return (
-    <div className="flex justify-center items-center pt-2 pl-1 pr-1">
-      <div className="w-full max-w-7xl relative">
-        <input
-          type="text"
-          placeholder="Search song.."
-          className="w-full p-2 pl-4 pr-10 border border-gray-300 rounded-full focus:outline-none focus:border-gray-500"
-        />
-        <button
-          onClick={() => console.log("search button clicked")}
-          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 focus:outline-none text-xl pr-3"
-        >
-          <IoSearch />
-        </button>
-      </div>
+    <div className="w-5/6 h-10 mx-auto my-4 px-6 rounded-full flex justify-between items-center bg-white">
+      <input
+        type="text"
+        placeholder="Madison Beer.."
+        className="outline-none border-none rounded-l-full"
+      />
+      <IoSearch />
     </div>
   );
 }
