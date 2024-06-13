11988dedcd676be8999ca47c61a18663e29ebdf4
Michelle Duineveld <michellemei29@gmail.com>
2024-06-13 10:03:14 +0200

    search bar component added

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,14 @@ import AlbumSlider from "@/components/Homepage/albumSlider";
 import PlaylistSlider from "@/components/Homepage/playlistSlider";
 import MusicPlayer from "@/components/musicPlayer";
 import SongSlider from "@/components/Homepage/songSlider.js";
+import SearchBar from "@/components/Homepage/searchBar";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-screen overflow-hidden  bg-[#6B6783] font-sans ">
+      <div className="p-3">
+        <SearchBar />
+      </div>
       <main className="flex-1 flex flex-col overflow-y-scroll">
         <div className="flex-1 min-h-screen">
           <SongSlider />
